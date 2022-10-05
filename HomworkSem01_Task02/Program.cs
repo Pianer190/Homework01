a71ddfbae16471f66a0901c6f8a60f6a9db23519
Dmitry <pianer190@mail.ru>
2022-10-05 15:36:38 +0300

    Добавлена задача

---
+++ HomworkSem01_Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введи 3 числа и узнай максимальное  ");
+
+int a = 1;
+int b = 2;
+int c = 6;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max =");
+Console.WriteLine(max);
