fd3601f99b9c9861be15a64a00a986dc0acd094e
Dmitry <pianer190@mail.ru>
2022-10-05 18:46:32 +0300

    Добавлена задача по определению чётного и нечётного числа

---
+++ HomeworkSem01_Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+int a;                             
+            Console.WriteLine("введите число: ");
+            a = Convert.ToInt32(Console.ReadLine()); 
+ 
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("четное число");
+           
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+ 
+ 
+ 
+            Console.ReadKey();
