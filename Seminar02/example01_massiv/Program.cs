28993bd91e3f56a01a027e14de4173708a63a781
Dmitry <pianer190@mail.ru>
2022-10-06 14:09:42 +0300

    пример нахождения максимума с помощью массива

---
+++ Seminar02/example01_massiv/Program.cs
@@ -0,0 +1,27 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//              0   1   2   3   4   5   6   7   8
+int[] array = { 11, 201, 31, 41, 15, 61, 17, 18, 19 };
+//array[0] = 12;     //Таким образом можно обратится к массиву и записать его значение
+//Console.WriteLine(array[0]); // Таким образом можно обратится к массиву и получить его значение соответвующего элемента поего индексу
+
+//int max1 = Max(
+//Max(array, b1, c1),
+//Max(a2, b2, c2),
+//Max(a3, b3, c3));
+
+
+//Поиск максимума из 9
+int max = Max(
+Max(array[0],array[1],array[2]),
+Max(array[3],array[4],array[5]),
+Max(array[6],array[7],array[8]));
+
+Console.WriteLine(max);
+
