e58130bb342a29705a2bb562b17d69bdee84bbfc
Dmitry <pianer190@mail.ru>
2022-10-06 15:56:52 +0300

    Пример применения метода void

---
+++ Seminar02/example03_metod/Program.cs
@@ -0,0 +1,34 @@
+﻿//метод void этот метод нечего не возвращает  при этом методе оператор returne использовать нельзя 
+void FillArray(int[] collection)
+
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+
+        collection[index] = new Random().Next(1, 10);
+        index++;
+
+    }
+
+}
+
+void FillArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+    }
+
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+printarray(array);
+//int[] array = new int[10]; // new int[10] конструкция , которая означает создай новый массив в котором будет 10 элементов 
