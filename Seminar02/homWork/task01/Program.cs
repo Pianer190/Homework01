87dcaf2e3df24e3cd124e21cb90c7c3fa18f75d0
Dmitry <pianer190@mail.ru>
2022-10-18 11:23:25 +0300

    Задача 1

---
+++ Seminar02/homWork/task01/Program.cs
@@ -0,0 +1,32 @@
+﻿// 01 здача Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для
+
+string tmp = "";
+int number = 0;
+
+Console.WriteLine("Введите трехначное число:");
+while(tmp.Length != 3){
+    tmp = Console.ReadLine();
+    if(!int.TryParse(tmp, out number)){
+        tmp = "";
+        Console.WriteLine("Введите трехначное число:");
+    }
+}
+
+
+do
+{
+    number = number / 10;
+    if(number < 100)
+    {
+        do
+        {
+            number = number - 10;
+            if (number < 10)
+            {
+                Console.WriteLine("Вторая (с начала) цифра : {0}", number);
+            }
+        }
+        while (number > 10);
+    }
+}
+while (number > 100);
