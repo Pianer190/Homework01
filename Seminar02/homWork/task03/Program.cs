8ff9bcda0eed1eb6a9dd6d511c112520e963ddff
Dmitry <pianer190@mail.ru>
2022-10-18 11:45:24 +0300

    Задача 3

---
+++ Seminar02/homWork/task03/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+
+string tmp = "";
+int number = 0;
+
+Console.WriteLine("Введите число:");
+while(tmp.Length < 3){
+    tmp = Console.ReadLine();
+    if(!int.TryParse(tmp, out number)){
+        tmp = "";
+        Console.WriteLine("Введите число:");
+    }
+}
+
+
+do
+{
+    number = number / 10;
+}
+while (number > 1000);
+
+Console.WriteLine("Остаток от деления: " + (number % 10));
