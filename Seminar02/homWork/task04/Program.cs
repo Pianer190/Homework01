7ec59a3164ed953523edded427a14645faa3ae1f
Dmitry <pianer190@mail.ru>
2022-10-18 10:46:43 +0300

    Задача 4

---
+++ Seminar02/homWork/task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+string tmp = Console.ReadLine();
+int day;
+var isValid = int.TryParse(tmp, out day);
+
+if(!isValid){
+    Console.WriteLine("Допускаются только числовые значения!");
+    return;
+}
+
+if(day < 1 || day > 7){
+    Console.WriteLine("День " + day + " не существует");
+    return;
+}
+if(day > 5) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
